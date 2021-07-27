75d24deae43ae407545ef8110e2d037f0b535cef
Ralixi <36295473+Ralixi@users.noreply.github.com>
2021-07-28 01:05:00 +0300

    add some bootstrap and basic navbar

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ TODO:
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-
+const morgan = require('morgan')
 const Campground = require('./models/campground');
-
-
 
 //connecting to db
 mongoose.connect('mongodb://localhost:27017/CampIL', {
@@ -29,11 +28,25 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.use(morgan('dev'))
+
+
+
+// app.get('/', async (req, res) => {
+//     res.render('./views/home')
+// })
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
