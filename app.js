037194773fb753dd9c9baa75b306517c88c6d3d1
Ralixi <36295473+Ralixi@users.noreply.github.com>
2021-08-15 14:20:46 +0300

    starting develope the features

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-/* 
-TODO:
-    1. create basic campground Schema
-    2. create connection to DB
-    
-*/
-
 const express = require('express');
-const mongoose = require('mongoose');
 const path = require('path');
+const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const morgan = require('morgan')
 const Campground = require('./models/campground');
 
-//connecting to db
-mongoose.connect('mongodb://localhost:27017/CampIL', {
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 });
 
-const db = mongoose.connection; //for shorter referance
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
@@ -39,15 +30,6 @@ app.use(methodOverride('_method'));
 app.get('/', (req, res) => {
     res.render('home')
 });
-
-app.use(morgan('dev'))
-
-
-
-// app.get('/', async (req, res) => {
-//     res.render('./views/home')
-// })
-
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
@@ -83,7 +65,9 @@ app.delete('/campgrounds/:id', async (req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 })
-//123
+
+
+
 app.listen(3000, () => {
-    console.log('Server listening on port 3000')
+    console.log('Serving on port 3000')
 })
