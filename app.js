ab2342de1acc8da8f09c5be794229d02cf6cb25a
Default Profile <evgenymarik@gmail.com>
2021-08-17 20:36:49 +0300

    add some user routes with login and register"

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
 
 const campgrounds = require('./routes/campgrounds');
@@ -46,6 +49,10 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig))
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
