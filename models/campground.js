80bb45de94dbb354274e957f0aede14d8f05be98
Default Profile <DefaultProfile@default.com>
2022-09-01 07:38:45 +0300

    fixing thumbnail prev photo at edit

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,19 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+//fixing path to 200 pixel size for thumbnail presentation
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
+
 const CampgroundSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
