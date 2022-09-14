52e35a96d298a2b39d5551bf84d33aa2b4f501c2
Default Profile <DefaultProfile@default.com>
2022-09-15 00:26:10 +0300

    Revert "fixing some stuff"
    This reverts commit 194efed2ede806dda2b0dea4be46444e2351020b.

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 const helmet = require("helmet");
+
 const MongoStore = require("connect-mongo");
+
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
@@ -22,27 +24,21 @@ const reviewRoutes = require("./routes/reviews");
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/CampIL";
 const secret = process.env.SECRET || "thisshouldbeabettersecret";
 
-const app = express();
-
-mongoose
-    .connect("mongodb://localhost:27017/CampIL", {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log(`connection to database established`);
-    })
-    .catch((err) => {
-        console.log(`db error ${err.message}`);
-        process.exit(-1);
-    });
+// mongoose.connect(dbUrl, {
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
+
+const app = express();
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
@@ -67,7 +63,7 @@ app.use(helmet());
 const sessionConfig = {
     store: MongoStore.create({
         mongoUrl: dbUrl,
-        dbName: "sessions-db",
+        dbName: "example-db",
         stringify: false,
     }),
     name: "session",
@@ -143,8 +139,12 @@ passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+/* 
+    passing glob. to middleware for inter-use    
+*/
 
 app.use((req, res, next) => {
+    // console.log(req.session)
     res.locals.currentUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
@@ -169,7 +169,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { err });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Serving on port ${port}`);
 });
--- controllers/users.js
+++ controllers/users.js
@@ -32,11 +32,7 @@ module.exports.login = (req, res) => {
 }
 
 module.exports.logout = (req, res) => {
-    req.logout((err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
+    req.logout();
     req.flash('success', "Goodbye!");
     res.redirect('/campgrounds');
 }
